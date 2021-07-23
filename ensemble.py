7b22dbfc3d488ba83ce4282c36c7105e118db513
kl <datesaver1234@gmail.com>
2021-07-23 16:19:19 +0900

    v6 model

--- ensemble.py
+++ ensemble.py
@@ -7,7 +7,7 @@ result1 = result1['Label'].to_list()
 result2 = pd.read_csv('result/result_9944.csv')
 result2 = result2['Label'].to_list()
 
-result3 = pd.read_csv('result/result_2.csv')
+result3 = pd.read_csv('result/result_9807_v5.csv')
 result3 = result3['Label'].to_list()
 
 acc = 0
@@ -17,4 +17,4 @@ for index, (a, b, c) in enumerate(zip(result1, result2, result3)):
     results.append([index + 1, x])
 
 df = pd.DataFrame(results, columns=['ImageId', 'Label'])
-df.to_csv('ensemble_result.csv', index=False)
+df.to_csv('result/ensemble_result.csv', index=False)
--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 result_csv = pd.read_csv('result/result_9946.csv')
 results = result_csv['Label'].to_list()
 
-test_csv = pd.read_csv('result/result.csv')
+test_csv = pd.read_csv('result/ensemble_result_2.csv')
 tests = test_csv['Label'].to_list()
 
 acc = 0
